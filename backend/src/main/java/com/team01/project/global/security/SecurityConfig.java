2496466a8ec27550d6d5ff7f76f80a4f122ed13d
엄현수 <55376152+sameom1048@users.noreply.github.com>
2025-03-13 15:11:08 +0900

    feat(35): CORS 설정 (#36)
    * :wrench: Chore: CORS 설정 추가
    
    * :recycle: Refactor: code style line 오류 수정

--- backend/src/main/java/com/team01/project/global/security/SecurityConfig.java
+++ backend/src/main/java/com/team01/project/global/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.team01.project.global.security;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +16,10 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.web.firewall.DefaultHttpFirewall;
 import org.springframework.security.web.firewall.HttpFirewall;
 import org.springframework.security.web.firewall.StrictHttpFirewall;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.team01.project.global.app.AppConfig;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -30,32 +36,33 @@ public class SecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtTokenFilter jwtTokenFilter) throws Exception {
 		System.out.println("======= START SeCurityFilterChain =======");
 		http
-			.securityMatcher("/**") // 모든 요청에 대해 보안 적용
-			.csrf(AbstractHttpConfigurer::disable)
-			.authorizeHttpRequests(
-				authorizeRequests -> authorizeRequests.requestMatchers("/api/v1/user/login", "/api/v1/logout",
-						"/api/v1/user/refresh", "/api/v1/error", "/login", "/")
-					.permitAll()
-					.anyRequest()
-					.authenticated()) // 모든 요청에 대해 인증 필요
-			//.anyRequest().permitAll()
-			.sessionManagement(
-				session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // JWT 사용 시 세션 비활성화
-			.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class) // JWT필터 추가 ( JWT token 검증 )
-			.oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
-					.successHandler(oAuth2SuccessHandler)
-					//	.defaultSuccessUrl("http://localhost:3000/login/callback", true) // 로그인 성공 시 리다이렉트 url
-					.failureUrl("/loginFailure")// 로그인 실패 시 리다이렉트 url
-				// .authorizationEndpoint(authorization ->
-				// 	authorization.baseUri("/oauth2/authorization")) // OAuth 인증 경로
-			)
-			.exceptionHandling(ex -> ex.authenticationEntryPoint(((request, response, authException) -> {
-				response.setContentType("application/json; charset=UTF-8");
-				response.setCharacterEncoding("UTF-8");
-				response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				response.getWriter().write("{\"error\": \"Unauthorized - 액세스 토큰이 없거나 로그인 인증되지 않았습니다.\"}");
-			})))
-			.setSharedObject(HttpFirewall.class, relaxedHttpFirewall());
+				.securityMatcher("/**") // 모든 요청에 대해 보안 적용
+				.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(
+						authorizeRequests -> authorizeRequests.requestMatchers("/api/v1/user/login", "/api/v1/logout",
+										"/api/v1/user/refresh", "/api/v1/error", "/login", "/")
+								.permitAll()
+								.anyRequest()
+								.authenticated()) // 모든 요청에 대해 인증 필요
+				//.anyRequest().permitAll()
+				.sessionManagement(
+						session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // JWT 사용 시 세션 비활성화
+				// JWT필터 추가 ( JWT token 검증 )
+				.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
+				.oauth2Login(oauth2 -> oauth2.userInfoEndpoint(userInfo -> userInfo.userService(oAuth2UserService))
+								.successHandler(oAuth2SuccessHandler)
+								//	.defaultSuccessUrl("http://localhost:3000/login/callback", true) // 로그인 성공 시 리다이렉트 url
+								.failureUrl("/loginFailure")// 로그인 실패 시 리다이렉트 url
+						// .authorizationEndpoint(authorization ->
+						// 	authorization.baseUri("/oauth2/authorization")) // OAuth 인증 경로
+				)
+				.exceptionHandling(ex -> ex.authenticationEntryPoint(((request, response, authException) -> {
+					response.setContentType("application/json; charset=UTF-8");
+					response.setCharacterEncoding("UTF-8");
+					response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+					response.getWriter().write("{\"error\": \"Unauthorized - 액세스 토큰이 없거나 로그인 인증되지 않았습니다.\"}");
+				})))
+				.setSharedObject(HttpFirewall.class, relaxedHttpFirewall());
 		return http.build();
 	}
 
@@ -71,5 +78,22 @@ public class SecurityConfig {
 		firewall.setAllowUrlEncodedPeriod(true); // . 허용
 		firewall.setAllowedHostnames(hostname -> true); // 모든 호스트 허용 (기본적으로 검사 비활성화)
 		return new DefaultHttpFirewall();
+	}
+
+	@Bean
+	public UrlBasedCorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		// 허용할 오리진 설정
+		configuration.setAllowedOrigins(Arrays.asList("https://cdpn.io", AppConfig.getSiteFrontUrl()));
+		// 허용할 HTTP 메서드 설정
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+		// 자격 증명 허용 설정
+		configuration.setAllowCredentials(true);
+		// 허용할 헤더 설정
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		// CORS 설정을 소스에 등록
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
