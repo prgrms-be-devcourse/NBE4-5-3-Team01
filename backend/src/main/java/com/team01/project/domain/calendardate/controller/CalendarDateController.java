c4ccc0ef8da3477029fd2b9667f18aeed4eb40dd
Wooseok Shin <104364275+shinwoos@users.noreply.github.com>
2025-03-17 23:50:34 +0900

    refactor#(86): 캘린더 swagger tag 적용 (#89)
    * refactor. 캘린더 swagger tag 적용
    
    * refactor. swagger tag 수정
    
    * feat. 린트 수정
    
    * refactor. 린트 수정

--- backend/src/main/java/com/team01/project/domain/calendardate/controller/CalendarDateController.java
+++ backend/src/main/java/com/team01/project/domain/calendardate/controller/CalendarDateController.java
@@ -30,8 +30,11 @@ import com.team01.project.domain.music.entity.Music;
 import com.team01.project.domain.musicrecord.entity.MusicRecord;
 import com.team01.project.domain.musicrecord.service.MusicRecordService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Calendar", description = "캘린더 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/calendar")
@@ -48,20 +51,18 @@ public class CalendarDateController {
 	 * @param loggedInUser 현재 인증된 유저
 	 * @return 먼슬리 캘린더
 	 */
+	@Operation(summary = "먼슬리 캘린더 조회 api", description = "현재 로그인 하고 있는 유저 또는 헤더의 Calendar-Own"
+		+ "er-Id와 동일한 아이디를 갖는 유저의 먼슬리 캘린더 조회")
 	@GetMapping(params = {"year", "month"})
 	@ResponseStatus(HttpStatus.OK)
-	public MonthlyFetchResponse fetchMonthlyCalendar(
-		@RequestParam int year,
-		@RequestParam int month,
+	public MonthlyFetchResponse fetchMonthlyCalendar(@RequestParam int year, @RequestParam int month,
 		@RequestHeader(name = "Calendar-Owner-Id", required = false) String ownerId,
-		@AuthenticationPrincipal OAuth2User loggedInUser
-	) {
+		@AuthenticationPrincipal OAuth2User loggedInUser) {
 		String loggedInUserId = loggedInUser.getName();
 		YearMonth yearMonth = YearMonth.of(year, month);
 
 		List<MonthlyFetchResponse.SingleCalendarDate> monthly = mapToMonthly(
-			calendarDateService.findAllByYearAndMonth(ownerId, loggedInUserId, yearMonth)
-		);
+			calendarDateService.findAllByYearAndMonth(ownerId, loggedInUserId, yearMonth));
 
 		return new MonthlyFetchResponse(monthly);
 	}
@@ -72,12 +73,11 @@ public class CalendarDateController {
 	 * @param loggedInUser 현재 인증된 유저
 	 * @return 캘린더
 	 */
+	@Operation(summary = "캘린더 조회 api", description = "현재 로그인 하고 있는 유저의 캘린더 조회")
 	@GetMapping("/{calendar-date-id}")
 	@ResponseStatus(HttpStatus.OK)
-	public CalendarDateFetchResponse fetchCalendarDate(
-		@PathVariable(name = "calendar-date-id") Long calendarDateId,
-		@AuthenticationPrincipal OAuth2User loggedInUser
-	) {
+	public CalendarDateFetchResponse fetchCalendarDate(@PathVariable(name = "calendar-date-id") Long calendarDateId,
+		@AuthenticationPrincipal OAuth2User loggedInUser) {
 		String loggedInUserId = loggedInUser.getName();
 
 		// CalendarDate 조회
@@ -98,15 +98,12 @@ public class CalendarDateController {
 	 * @param loggedInUser 현재 인증된 유저
 	 * @return 생성된 캘린더 아이디
 	 */
+	@Operation(summary = "캘린더 생성 api", description = "현재 로그인 하고 있는 유저의 캘린더 생성")
 	@PostMapping(params = {"year", "month", "day"})
 	@ResponseStatus(HttpStatus.CREATED)
-	public CalendarDateCreateResponse createCalendarDate(
-		@RequestParam int year,
-		@RequestParam int month,
-		@RequestParam int day,
-		@RequestBody CalendarDateCreateRequest request,
-		@AuthenticationPrincipal OAuth2User loggedInUser
-	) {
+	public CalendarDateCreateResponse createCalendarDate(@RequestParam int year, @RequestParam int month,
+		@RequestParam int day, @RequestBody CalendarDateCreateRequest request,
+		@AuthenticationPrincipal OAuth2User loggedInUser) {
 		String loggedInUserId = loggedInUser.getName();
 		LocalDate date = LocalDate.of(year, month, day);
 
@@ -127,13 +124,11 @@ public class CalendarDateController {
 	 * @param request 음악 아이디 리스트
 	 * @param loggedInUser 현재 인증된 유저
 	 */
+	@Operation(summary = "작성된 음악 기록 수정 api", description = "현재 로그인 하고 있는 유저의 음악 기록 수정할때만 사용")
 	@PostMapping("/{calendar-date-id}/music")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void saveMusicToCalendarDate(
-		@PathVariable(name = "calendar-date-id") Long calendarDateId,
-		@RequestBody CalendarDateMusicSaveRequest request,
-		@AuthenticationPrincipal OAuth2User loggedInUser
-	) {
+	public void saveMusicToCalendarDate(@PathVariable(name = "calendar-date-id") Long calendarDateId,
+		@RequestBody CalendarDateMusicSaveRequest request, @AuthenticationPrincipal OAuth2User loggedInUser) {
 		String loggedInUserId = loggedInUser.getName();
 		musicRecordService.updateMusicRecords(calendarDateId, loggedInUserId, request.musicIds());
 	}
@@ -144,28 +139,24 @@ public class CalendarDateController {
 	 * @param request 새로운 메모
 	 * @param loggedInUser 현재 인증된 유저
 	 */
+	@Operation(summary = "작성된 메모 수정 api", description = "현재 로그인 하고 있는 유저의 메모 작성 수정할때만 사용")
 	@PostMapping("/{calendar-date-id}/memo")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void writeMemoToCalendarDate(
-		@PathVariable(name = "calendar-date-id") Long calendarDateId,
-		@RequestBody CalendarDateMemoSaveRequest request,
-		@AuthenticationPrincipal OAuth2User loggedInUser
-	) {
+	public void writeMemoToCalendarDate(@PathVariable(name = "calendar-date-id") Long calendarDateId,
+		@RequestBody CalendarDateMemoSaveRequest request, @AuthenticationPrincipal OAuth2User loggedInUser) {
 		String loggedInUserId = loggedInUser.getName();
 		calendarDateService.writeMemo(calendarDateId, loggedInUserId, request.memo());
 	}
 
 	private List<MonthlyFetchResponse.SingleCalendarDate> mapToMonthly(List<CalendarDate> calendarDates) {
-		return calendarDates.stream()
-			.map(this::mapToSingleCalendarDate)
-			.toList();
+		return calendarDates.stream().map(this::mapToSingleCalendarDate).toList();
 	}
 
 	private MonthlyFetchResponse.SingleCalendarDate mapToSingleCalendarDate(CalendarDate calendarDate) {
 		Optional<MusicRecord> optionalMusicRecord = musicRecordService.findOneByCalendarDateId(calendarDate.getId());
 
-		return optionalMusicRecord
-			.map(musicRecord -> MonthlyFetchResponse.SingleCalendarDate.of(calendarDate, musicRecord.getMusic()))
+		return optionalMusicRecord.map(
+				musicRecord -> MonthlyFetchResponse.SingleCalendarDate.of(calendarDate, musicRecord.getMusic()))
 			.orElseGet(() -> MonthlyFetchResponse.SingleCalendarDate.from(calendarDate));
 	}
 
