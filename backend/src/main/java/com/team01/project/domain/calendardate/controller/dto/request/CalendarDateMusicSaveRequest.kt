fbc1ee6174c9a2f1ab9d3a8bb741ac0ad9cd9585
jisukimtech <jisukim.tech@gmail.com>
2025-04-07 12:45:26 +0900

    Refactor(DTO): 음악 기록 수정 요청 DTO 마이그레이션

--- backend/src/main/java/com/team01/project/domain/calendardate/controller/dto/request/CalendarDateMusicSaveRequest.kt
+++ backend/src/main/java/com/team01/project/domain/calendardate/controller/dto/request/CalendarDateMusicSaveRequest.kt
@@ -1,12 +1,10 @@
-package com.team01.project.domain.calendardate.controller.dto.request;
-
-import java.util.List;
+package com.team01.project.domain.calendardate.controller.dto.request
 
 /**
- * 음악 기록 저장 요청 DTO
+ * 음악 기록 수정 요청 DTO
  * @param musicIds 해당 캘린더에 기록할 전체 음악 아이디 리스트
  */
-public record CalendarDateMusicSaveRequest(
-	List<String> musicIds
-) {
-}
+
+data class CalendarDateMusicSaveRequest(
+    val musicIds: List<String>
+) 
