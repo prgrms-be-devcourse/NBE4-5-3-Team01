651ade6636968ecc4873022532666c3991b0a695
jisukimtech <jisukim.tech@gmail.com>
2025-04-07 12:27:26 +0900

    Refactor(DTO): 캘린더 생성 요청 DTO 마이그레이션

--- backend/src/main/java/com/team01/project/domain/calendardate/controller/dto/request/CalendarDateCreateRequest.kt
+++ backend/src/main/java/com/team01/project/domain/calendardate/controller/dto/request/CalendarDateCreateRequest.kt
@@ -1,23 +1,11 @@
-package com.team01.project.domain.calendardate.controller.dto.request;
-
-import java.util.List;
+package com.team01.project.domain.calendardate.controller.dto.request
 
 /**
  * 캘린더 생성 요청 DTO
  * @param memo 메모
  * @param musicIds 음악 아이디 리스트
  */
-public record CalendarDateCreateRequest(
-	String memo,
-	List<String> musicIds
-) {
-
-	public CalendarDateCreateRequest(
-		String memo,
-		List<String> musicIds
-	) {
-		this.memo = memo == null ? "" : memo;
-		this.musicIds = musicIds == null ? List.of() : musicIds;
-	}
-
-}
+data class CalendarDateCreateRequest(
+    val memo: String = "",
+    val musicIds: List<String> = emptyList()
+)
