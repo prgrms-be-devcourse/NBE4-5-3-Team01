6cc4fbe9be9143b44bfd0ee40fddccedf8fa1aab
jisukimtech <jisukim.tech@gmail.com>
2025-04-07 11:37:31 +0900

    Refactor(Entity): 캘린더 엔티티 마이그레이션

--- backend/src/main/java/com/team01/project/domain/calendardate/entity/CalendarDate.kt
+++ backend/src/main/java/com/team01/project/domain/calendardate/entity/CalendarDate.kt
@@ -1,47 +1,28 @@
-package com.team01.project.domain.calendardate.entity;
+package com.team01.project.domain.calendardate.entity
 
-import java.time.LocalDate;
-
-import com.team01.project.domain.user.entity.User;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.team01.project.domain.user.entity.User
+import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
-public class CalendarDate {
+class CalendarDate(
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "user_id", nullable = false)
-	private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    val user: User,
 
-	@Column(nullable = false)
-	private LocalDate date;
+    @Column(nullable = false)
+    val date: LocalDate,
 
-	@Column(columnDefinition = "TEXT", nullable = false)
-	@Builder.Default
-	private String memo = "";
+    @Column(columnDefinition = "TEXT", nullable = false)
+    var memo: String = ""
 
-	public void writeMemo(String memo) {
-		this.memo = memo;
-	}
-
+) {
+    fun writeMemo(memo: String) {
+        this.memo = memo
+    }
 }
