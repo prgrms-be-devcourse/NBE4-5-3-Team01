e90391aa707dff9474054689a0e8432b8111a0c5
jisukimtech <jisukim.tech@gmail.com>
2025-04-07 16:02:59 +0900

    Fix(KtLint): 와일드 카드 임포트 삭제

--- backend/src/main/java/com/team01/project/domain/calendardate/controller/CalendarDateController.kt
+++ backend/src/main/java/com/team01/project/domain/calendardate/controller/CalendarDateController.kt
@@ -15,7 +15,16 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.security.oauth2.core.user.OAuth2User
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
 import java.time.YearMonth
 
--- backend/src/main/java/com/team01/project/domain/calendardate/entity/CalendarDate.kt
+++ backend/src/main/java/com/team01/project/domain/calendardate/entity/CalendarDate.kt
@@ -1,7 +1,14 @@
 package com.team01.project.domain.calendardate.entity
 
 import com.team01.project.domain.user.entity.User
-import jakarta.persistence.*
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import java.time.LocalDate
 
 @Entity
