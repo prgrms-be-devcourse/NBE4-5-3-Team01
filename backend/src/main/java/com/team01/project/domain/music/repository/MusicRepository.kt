3a2312a01afcd7eb0babc8a945a1c0b117eb2f93
jxxngin <wwd0203@daum.net>
2025-04-07 11:47:39 +0900

    Refactor: Migrate Repository from java to kt

--- backend/src/main/java/com/team01/project/domain/music/repository/MusicRepository.kt
+++ backend/src/main/java/com/team01/project/domain/music/repository/MusicRepository.kt
@@ -1,16 +1,14 @@
-package com.team01.project.domain.music.repository;
+package com.team01.project.domain.music.repository
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.team01.project.domain.music.entity.Music
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.data.jpa.repository.JpaRepository
 
-import com.team01.project.domain.music.entity.Music;
+interface MusicRepository : JpaRepository<Music, String> {
 
-import jakarta.persistence.EntityNotFoundException;
-
-public interface MusicRepository extends JpaRepository<Music, String> {
-
-	default Music findByIdOrThrow(String id) {
-		return findById(id).orElseThrow(()
-			-> new EntityNotFoundException("해당 ID의 음악을 찾을 수 없습니다: " + id));
-	}
-
+    fun findByIdOrThrow(id: String): Music {
+        return findById(id).orElseThrow {
+            EntityNotFoundException("해당 ID의 음악을 찾을 수 없습니다: $id")
+        }
+    }
 }
