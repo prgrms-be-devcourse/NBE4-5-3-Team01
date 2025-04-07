2edb7ed83da8e1544dd6e80ec4839b7b9a2668f5
jisukimtech <jisukim.tech@gmail.com>
2025-04-07 17:33:32 +0900

    Refactor(Entity): 음악 기록 엔티티 마이그레이션

--- backend/src/main/java/com/team01/project/domain/musicrecord/entity/MusicRecord.kt
+++ backend/src/main/java/com/team01/project/domain/musicrecord/entity/MusicRecord.kt
@@ -1,36 +1,28 @@
-package com.team01.project.domain.musicrecord.entity;
+package com.team01.project.domain.musicrecord.entity
 
-import com.team01.project.domain.calendardate.entity.CalendarDate;
-import com.team01.project.domain.music.entity.Music;
-
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.team01.project.domain.calendardate.entity.CalendarDate
+import com.team01.project.domain.music.entity.Music
+import jakarta.persistence.EmbeddedId
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.MapsId
 
 @Entity
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class MusicRecord {
+class MusicRecord(
 
-	@EmbeddedId
-	private MusicRecordId id;
+    @EmbeddedId
+    val id: MusicRecordId,
 
-	@MapsId("calendarDateId")
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "calendar_date_id", nullable = false)
-	private CalendarDate calendarDate;
+    @MapsId("calendarDateId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "calendar_date_id", nullable = false)
+    val calendarDate: CalendarDate,
 
-	@MapsId("musicId")
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "music_id", nullable = false)
-	private Music music;
+    @MapsId("musicId")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "music_id", nullable = false)
+    val music: Music
 
-}
+)
