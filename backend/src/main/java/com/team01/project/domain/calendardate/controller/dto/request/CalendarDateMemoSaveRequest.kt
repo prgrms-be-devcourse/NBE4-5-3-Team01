9b7d21f5d782bbb79967fa5d211fc394f59ccfcc
jisukimtech <jisukim.tech@gmail.com>
2025-04-07 12:38:53 +0900

    Refactor(DTO): 메모 수정 요청 DTO 마이그레이션

--- backend/src/main/java/com/team01/project/domain/calendardate/controller/dto/request/CalendarDateMemoSaveRequest.kt
+++ backend/src/main/java/com/team01/project/domain/calendardate/controller/dto/request/CalendarDateMemoSaveRequest.kt
@@ -1,15 +1,14 @@
-package com.team01.project.domain.calendardate.controller.dto.request;
+package com.team01.project.domain.calendardate.controller.dto.request
 
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Size
 
 /**
- * 메모 작성 요청 DTO
+ * 메모 수정 요청 DTO
  * @param memo 새로운 메모
  */
-public record CalendarDateMemoSaveRequest(
+data class CalendarDateMemoSaveRequest(
 
-	@Size(max = 1000, message = "메모는 1000자 이내로 입력해주세요.")
-	String memo
+    @field:Size(max = 1000, message = "메모는 1000자 이내로 입력해주세요.")
+    val memo: String = "",
 
-) {
-}
+    )
