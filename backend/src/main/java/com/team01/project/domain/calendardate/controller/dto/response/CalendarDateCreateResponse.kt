4b4519e1edadb71480645930ba02fa25f1048c0e
jisukimtech <jisukim.tech@gmail.com>
2025-04-07 12:47:07 +0900

    Refactor(DTO): 캘린더 생성 응답 DTO 마이그레이션

--- backend/src/main/java/com/team01/project/domain/calendardate/controller/dto/response/CalendarDateCreateResponse.kt
+++ backend/src/main/java/com/team01/project/domain/calendardate/controller/dto/response/CalendarDateCreateResponse.kt
@@ -1,10 +1,9 @@
-package com.team01.project.domain.calendardate.controller.dto.response;
+package com.team01.project.domain.calendardate.controller.dto.response
 
 /**
  * 캘린더 생성 응답 DTO
  * @param id 생성된 캘린더 아이디
  */
-public record CalendarDateCreateResponse(
-	Long id
-) {
-}
+data class CalendarDateCreateResponse(
+    val id: Long
+) 
