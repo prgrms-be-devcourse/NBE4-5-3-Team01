ee0a5bc3cf3df87c34f4a0224ebfd6c73bd70e8e
jisukimtech <jisukim.tech@gmail.com>
2025-04-08 14:05:20 +0900

    Fix(KtLint): 불필요한 공백 제거

--- backend/src/main/java/com/team01/project/domain/user/entity/CalendarVisibility.kt
+++ backend/src/main/java/com/team01/project/domain/user/entity/CalendarVisibility.kt
@@ -4,7 +4,7 @@ package com.team01.project.domain.user.entity
  * 캘린더 공개 여부
  */
 enum class CalendarVisibility {
-    PUBLIC,  // 전체 공개
-    FOLLOWER_ONLY,  // 팔로워 공개
+    PUBLIC, // 전체 공개
+    FOLLOWER_ONLY, // 팔로워 공개
     PRIVATE // 비공개
 }
