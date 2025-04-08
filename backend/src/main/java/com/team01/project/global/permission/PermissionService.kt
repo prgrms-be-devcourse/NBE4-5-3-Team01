c32089164e07d0eae224fb94e1dab6d04d470516
jisukimtech <jisukim.tech@gmail.com>
2025-04-08 12:43:45 +0900

    Refactor(Service): 권한 서비스 마이그레이션

--- backend/src/main/java/com/team01/project/global/permission/PermissionService.kt
+++ backend/src/main/java/com/team01/project/global/permission/PermissionService.kt
@@ -1,96 +1,71 @@
-package com.team01.project.global.permission;
+package com.team01.project.global.permission
 
-import static com.team01.project.domain.follow.entity.type.Status.*;
-import static com.team01.project.domain.user.entity.CalendarVisibility.*;
-import static com.team01.project.global.permission.CalendarPermission.*;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import com.team01.project.domain.follow.entity.type.Status;
-import com.team01.project.domain.follow.repository.FollowRepository;
-import com.team01.project.domain.user.entity.User;
-
-import lombok.RequiredArgsConstructor;
+import com.team01.project.domain.follow.entity.type.Status
+import com.team01.project.domain.follow.repository.FollowRepository
+import com.team01.project.domain.user.entity.CalendarVisibility
+import com.team01.project.domain.user.entity.User
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-@RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class PermissionService {
+class PermissionService(
+    private val followRepository: FollowRepository
+) {
+    /**
+     * 캘린더 권한 확인
+     * @param calendarOwner 캘린더 소유자
+     * @param loggedInUser 현재 인증된 유저
+     * @return 캘린더 권한
+     */
+    fun checkPermission(calendarOwner: User, loggedInUser: User): CalendarPermission {
+        return when {
+            hasEditPermission(calendarOwner, loggedInUser) -> CalendarPermission.EDIT
+            hasViewPermission(calendarOwner, loggedInUser) -> CalendarPermission.VIEW
+            else -> CalendarPermission.NONE
+        }
+    }
 
-	private final FollowRepository followRepository;
+    /**
+     * 캘린더 조회 가능 여부 확인
+     * @param calendarOwner 캘린더 소유자
+     * @param loggedInUser 현재 인증된 유저
+     * @return 캘린더 조회 가능 여부
+     */
+    private fun hasViewPermission(calendarOwner: User, loggedInUser: User): Boolean {
+        // 조회 가능한 경우
+        // [1] 전체 공개인 경우
+        // [2] 팔로워 공개인 경우 현재 인증된 유저가 캘린더 소유자의 팔로워라면 조회 가능
+        // [3] 비공개인 경우 현재 인증된 유저가 캘린더 소유자라면 조회 가능
+        return isPublicCalendar(calendarOwner) ||
+                (isFollowerOnlyCalendar(calendarOwner) && isFollower(loggedInUser, calendarOwner)) ||
+                isCalendarOwner(calendarOwner, loggedInUser)
+    }
 
-	/**
-	 * 캘린더 권한 확인
-	 * @param calendarOwner 캘린더 소유자
-	 * @param loggedInUser 현재 인증된 유저
-	 * @return 캘린더 권한
-	 */
-	public CalendarPermission checkPermission(User calendarOwner, User loggedInUser) {
-		if (hasEditPermission(calendarOwner, loggedInUser)) {
-			return EDIT;
-		}
+    /**
+     * 캘린더 수정 가능 여부 확인
+     * @param calendarOwner 캘린더 소유자
+     * @param loggedInUser 현재 인증된 유저
+     * @return 캘린더 수정 가능 여부
+     */
+    private fun hasEditPermission(calendarOwner: User, loggedInUser: User): Boolean {
+        return isCalendarOwner(calendarOwner, loggedInUser)
+    }
 
-		if (hasViewPermission(calendarOwner, loggedInUser)) {
-			return VIEW;
-		}
+    private fun isPublicCalendar(calendarOwner: User): Boolean {
+        return CalendarVisibility.PUBLIC == calendarOwner.calendarVisibility
+    }
 
-		return CalendarPermission.NONE;
-	}
+    private fun isFollowerOnlyCalendar(calendarOwner: User): Boolean {
+        return CalendarVisibility.FOLLOWER_ONLY == calendarOwner.calendarVisibility
+    }
 
-	/**
-	 * 캘린더 조회 가능 여부 확인
-	 * @param calendarOwner 캘린더 소유자
-	 * @param loggedInUser 현재 인증된 유저
-	 * @return 캘린더 조회 가능 여부
-	 */
-	private boolean hasViewPermission(User calendarOwner, User loggedInUser) {
+    private fun isFollower(fromUser: User, toUser: User): Boolean {
+        return Status.ACCEPT == followRepository.findStatusByToUserAndFromUser(toUser, fromUser).orElse(Status.NONE)
+    }
 
-		// 캘린더가 전체 공개로 설정돼있는 경우 조회 가능
-		if (isPublicCalendar(calendarOwner)) {
-			return true;
-		}
-
-		// 캘린더가 팔로워 공개로 설정돼있는 경우 현재 인증된 유저가 캘린더 소유자의 팔로워라면 조회 가능
-		if (isFollowerOnlyCalendar(calendarOwner) && isFollower(loggedInUser, calendarOwner)) {
-			return true;
-		}
-
-		// 현재 인증된 유저가 캘린더 소유자인 경우 조회 가능
-		if (isCalendarOwner(calendarOwner, loggedInUser)) {
-			return true;
-		}
-
-		// 이외의 경우에는 조회 불가능
-		return false;
-
-	}
-
-	/**
-	 * 캘린더 수정 가능 여부 확인
-	 * @param calendarOwner 캘린더 소유자
-	 * @param loggedInUser 현재 인증된 유저
-	 * @return 캘린더 수정 가능 여부
-	 */
-	private boolean hasEditPermission(User calendarOwner, User loggedInUser) {
-		return isCalendarOwner(calendarOwner, loggedInUser);
-	}
-
-	private boolean isPublicCalendar(User calendarOwner) {
-		return PUBLIC == calendarOwner.getCalendarVisibility();
-	}
-
-	private boolean isFollowerOnlyCalendar(User calendarOwner) {
-		return FOLLOWER_ONLY == calendarOwner.getCalendarVisibility();
-	}
-
-	private boolean isFollower(User fromUser, User toUser) {
-		return ACCEPT == followRepository.findStatusByToUserAndFromUser(toUser, fromUser).orElse(Status.NONE);
-
-	}
-
-	private boolean isCalendarOwner(User calendarOwner, User loggedInUser) {
-		return calendarOwner.equals(loggedInUser);
-	}
-
+    private fun isCalendarOwner(calendarOwner: User, loggedInUser: User): Boolean {
+        return calendarOwner == loggedInUser // ==: .equals() 호출 -> 내용 비교
+                                             // ===: 참조 비교
+    }
 }
