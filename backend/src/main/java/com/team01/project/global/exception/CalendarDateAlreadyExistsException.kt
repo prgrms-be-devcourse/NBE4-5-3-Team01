e17e736b4ba8ebb0157694055269b87f10901587
jisukimtech <jisukim.tech@gmail.com>
2025-04-08 10:40:11 +0900

    Refactor(Exception): CalendarDateAlreadyExistsException 마이그레이션

--- backend/src/main/java/com/team01/project/global/exception/CalendarDateAlreadyExistsException.kt
+++ backend/src/main/java/com/team01/project/global/exception/CalendarDateAlreadyExistsException.kt
@@ -1,13 +1,6 @@
-package com.team01.project.global.exception;
+package com.team01.project.global.exception
 
-import lombok.Getter;
-
-@Getter
-public class CalendarDateAlreadyExistsException extends RuntimeException {
-	private final String statusCode;
-
-	public CalendarDateAlreadyExistsException(String statusCode, String message) {
-		super(message);
-		this.statusCode = statusCode;
-	}
-}
+class CalendarDateAlreadyExistsException(
+    val statusCode: String,
+    message: String
+) : RuntimeException(message)
