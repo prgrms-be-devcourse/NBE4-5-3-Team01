59266f9dadb9058aa0642b7d5354441f11b4a2e4
jisukimtech <jisukim.tech@gmail.com>
2025-04-08 20:43:52 +0900

    Fix(KtLint): 불필요한 블럭 제거

--- backend/src/main/java/com/team01/project/domain/music/repository/MusicRepository.kt
+++ backend/src/main/java/com/team01/project/domain/music/repository/MusicRepository.kt
@@ -3,5 +3,4 @@ package com.team01.project.domain.music.repository
 import com.team01.project.domain.music.entity.Music
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface MusicRepository : JpaRepository<Music, String> {
-}
+interface MusicRepository : JpaRepository<Music, String>
