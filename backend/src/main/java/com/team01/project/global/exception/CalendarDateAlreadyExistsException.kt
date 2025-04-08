9f4a8594215a9c0164f78fe399d356ffc84bd517
jisukimtech <jisukim.tech@gmail.com>
2025-04-08 11:12:23 +0900

    Feat(Exception): 커스텀 예외에 non-null message 필드 추가
    - 커스텀 예외가 상속받는 RuntimeException의 message는 String? 타입
    - 외부에서 접근한 커스텀 예외의 message도 String? 타입
    - 따라서 message를 저장하는 non-null 필드를 추가함

--- backend/src/main/java/com/team01/project/global/exception/CalendarDateAlreadyExistsException.kt
+++ backend/src/main/java/com/team01/project/global/exception/CalendarDateAlreadyExistsException.kt
@@ -3,4 +3,6 @@ package com.team01.project.global.exception
 class CalendarDateAlreadyExistsException(
     val statusCode: String,
     message: String
-) : RuntimeException(message)
+) : RuntimeException(message) {
+    val nonNullMessage: String = message
+}
