d5d23933198d8f8a9eae03b6515f93a8e34179e7
jisukimtech <jisukim.tech@gmail.com>
2025-04-08 10:47:45 +0900

    Refactor(Exception): PermissionDeniedException 마이그레이션

--- backend/src/main/java/com/team01/project/global/exception/PermissionDeniedException.kt
+++ backend/src/main/java/com/team01/project/global/exception/PermissionDeniedException.kt
@@ -1,13 +1,6 @@
-package com.team01.project.global.exception;
+package com.team01.project.global.exception
 
-import lombok.Getter;
-
-@Getter
-public class PermissionDeniedException extends RuntimeException {
-	private final String statusCode;
-
-	public PermissionDeniedException(String statusCode, String message) {
-		super(message);
-		this.statusCode = statusCode;
-	}
-}
+class PermissionDeniedException(
+    val statusCode: String,
+    message: String
+) : RuntimeException(message)
