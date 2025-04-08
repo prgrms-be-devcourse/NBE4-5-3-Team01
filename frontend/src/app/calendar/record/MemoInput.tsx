f115f5fee9951be00a399d6f81e0c59ac13a5665
jxxngin <wwd0203@daum.net>
2025-04-08 20:27:19 +0900

    Feat: 멤버십 등급에 따라 기록, 메모 글자 갯수 제한

--- frontend/src/app/calendar/record/MemoInput.tsx
+++ frontend/src/app/calendar/record/MemoInput.tsx
@@ -2,13 +2,14 @@
 
 import "./style.css";
 
-export default function MemoInput({ memo, setMemo }) {
+export default function MemoInput({ memo, setMemo, maxLength }) {
   return (
     <div>
       <h3 className="text-xl font-semibold mb-2 ml-2">메모</h3>
       <textarea
         className="memo-input"
         placeholder="메모를 입력하세요."
+        maxLength={maxLength}
         value={memo}
         onChange={(e) => setMemo(e.target.value)}
       />
