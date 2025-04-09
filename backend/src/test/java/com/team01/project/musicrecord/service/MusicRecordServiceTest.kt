59ffca1fce0d6a4bfcd657a6424d4f5915f46530
jisukimtech <jisukim.tech@gmail.com>
2025-04-09 17:40:30 +0900

    Refactor(MusicRecordServiceTest): 마이그레이션

--- backend/src/test/java/com/team01/project/musicrecord/service/MusicRecordServiceTest.kt
+++ backend/src/test/java/com/team01/project/musicrecord/service/MusicRecordServiceTest.kt
@@ -1,137 +1,145 @@
-package com.team01.project.musicrecord.service;
+package com.team01.project.musicrecord.service
 
-import static com.team01.project.global.permission.CalendarPermission.*;
-import static org.assertj.core.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import com.team01.project.domain.calendardate.entity.CalendarDate
+import com.team01.project.domain.calendardate.repository.CalendarDateRepository
+import com.team01.project.domain.music.entity.Music
+import com.team01.project.domain.music.repository.MusicRepository
+import com.team01.project.domain.musicrecord.entity.MusicRecord
+import com.team01.project.domain.musicrecord.entity.MusicRecordId
+import com.team01.project.domain.musicrecord.repository.MusicRecordRepository
+import com.team01.project.domain.musicrecord.service.MusicRecordService
+import com.team01.project.domain.user.entity.User
+import com.team01.project.domain.user.repository.UserRepository
+import com.team01.project.global.permission.CalendarPermission
+import com.team01.project.global.permission.PermissionService
+import com.team01.project.util.TestDisplayNameGenerator
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.DisplayNameGeneration
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.util.Optional
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.team01.project.domain.calendardate.entity.CalendarDate;
-import com.team01.project.domain.calendardate.repository.CalendarDateRepository;
-import com.team01.project.domain.music.entity.Music;
-import com.team01.project.domain.music.repository.MusicRepository;
-import com.team01.project.domain.musicrecord.entity.MusicRecord;
-import com.team01.project.domain.musicrecord.entity.MusicRecordId;
-import com.team01.project.domain.musicrecord.repository.MusicRecordRepository;
-import com.team01.project.domain.musicrecord.service.MusicRecordService;
-import com.team01.project.domain.user.entity.User;
-import com.team01.project.domain.user.repository.UserRepository;
-import com.team01.project.global.permission.PermissionService;
-
-@ExtendWith(MockitoExtension.class)
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayNameGeneration(TestDisplayNameGenerator::class)
 class MusicRecordServiceTest {
+    private val musicRecordRepository: MusicRecordRepository = mockk()
+    private val calendarDateRepository: CalendarDateRepository = mockk()
+    private val musicRepository: MusicRepository = mockk()
+    private val userRepository: UserRepository = mockk()
+    private val permissionService: PermissionService = mockk()
 
-	@Mock
-	private MusicRecordRepository musicRecordRepository;
+    private val musicRecordService = MusicRecordService(
+        musicRecordRepository,
+        calendarDateRepository,
+        musicRepository,
+        userRepository,
+        permissionService
+    )
 
-	@Mock
-	private CalendarDateRepository calendarDateRepository;
+    private val calendarDateId = 1L
+    private val userId = "test-user"
 
-	@Mock
-	private MusicRepository musicRepository;
+    private val user = User(id = userId, email = "email", name = "name")
 
-	@Mock
-	private UserRepository userRepository;
+    private val calendarDate = CalendarDate(
+        id = calendarDateId,
+        user = user,
+        date = LocalDate.of(2025, 3, 1),
+        memo = "memo 1"
+    )
 
-	@Mock
-	private PermissionService permissionService;
+    private val music1 = Music(
+        id = "1",
+        name = "song 1",
+        singer = "singer 1",
+        singerId = "singer 1 id",
+        albumImage = "album image 1",
+    )
 
-	@InjectMocks
-	private MusicRecordService musicRecordService;
+    private val music2 = Music(
+        id = "2",
+        name = "song 2",
+        singer = "singer 2",
+        singerId = "singer 2 id",
+        albumImage = "album image 2",
+    )
 
-	private Long calendarDateId;
-	private CalendarDate calendarDate;
-	private List<MusicRecord> musicRecords;
-	private String userId;
-	private User user;
+    private val record1 = MusicRecord(
+        id = MusicRecordId(calendarDateId, "1"),
+        calendarDate = calendarDate,
+        music = music1
+    )
 
-	@BeforeEach
-	void 초기화() {
-		userId = "test-user";
-		user = User.builder().id(userId).build();
+    private val record2 = MusicRecord(
+        id = MusicRecordId(calendarDateId, "2"),
+        calendarDate = calendarDate,
+        music = music2
+    )
 
-		calendarDateId = 1L;
-		calendarDate = CalendarDate.builder().id(calendarDateId).user(user).build();
+    private val musicRecords = listOf(record1, record2)
 
-		Music music1 = Music.builder().id("1").name("Song 1").build();
-		Music music2 = Music.builder().id("2").name("Song 2").build();
+    @Test
+    fun `기록한 음악 목록을 캘린더 아이디로 조회한다`() {
+        // given
+        every { calendarDateRepository.findById(calendarDateId) } returns Optional.of(calendarDate)
+        every { musicRecordRepository.findByCalendarDate(calendarDate) } returns musicRecords
 
-		MusicRecord record1 = new MusicRecord(new MusicRecordId(calendarDateId, "1"), calendarDate, music1);
-		MusicRecord record2 = new MusicRecord(new MusicRecordId(calendarDateId, "2"), calendarDate, music2);
+        // when
+        val result = musicRecordService.findMusicsByCalendarDateId(calendarDateId)
 
-		musicRecords = List.of(record1, record2);
-	}
+        // then
+        assertThat(result).hasSize(musicRecords.size)
+        assertTrue(result.contains(music1))
+        assertTrue(result.contains(music2))
+    }
 
-	@Test
-	void 기록한_음악_목록을_캘린더_아이디로_조회한다() {
+    @Test
+    fun `음악 기록 하나를 캘린더 아이디로 조회한다`() {
+        // given
+        every { calendarDateRepository.findById(calendarDateId) } returns Optional.of(calendarDate)
+        every { musicRecordRepository.findTopByCalendarDate(calendarDate) } returns Optional.of(musicRecords[0])
 
-		// given
-		when(calendarDateRepository.findByIdOrThrow(calendarDateId)).thenReturn(calendarDate);
-		when(musicRecordRepository.findByCalendarDate(calendarDate)).thenReturn(musicRecords);
+        // when
+        val result = musicRecordService.findOneByCalendarDateId(calendarDateId)
 
-		// when
-		List<Music> result = musicRecordService.findMusicsByCalendarDateId(calendarDateId);
+        // then
+        assertThat(result.isPresent)
+        assertThat(result.get().music.id).isEqualTo(musicRecords[0].music.id)
+    }
 
-		// then
-		assertThat(result).hasSize(musicRecords.size());
-		assertThat(result.stream().map(Music::getId).collect(Collectors.toSet()))
-			.containsExactlyInAnyOrder("1", "2");
+    @Test
+    fun `음악 기록을 업데이트한다`() {
+        // given
+        val commonMusicId: String = musicRecords[0].music.id
+        val musicIdToAdd = "3"
+        val musicNameToAdd = "Song 3"
+        val newMusicIds = listOf(commonMusicId, musicIdToAdd)
+        val musicToAdd = Music(
+            id = musicIdToAdd,
+            name = musicNameToAdd,
+            singer = "singer 3",
+            singerId = "singer 3 id",
+            albumImage = "album image 3",
+        )
 
-	}
+        every { calendarDateRepository.findWithOwnerById(calendarDateId) } returns Optional.of(calendarDate)
+        every { userRepository.findById(userId) } returns Optional.of(user)
+        every { permissionService.checkPermission(any<User>(), any<User>()) } returns CalendarPermission.EDIT
+        every { musicRecordRepository.findByCalendarDate(calendarDate) } returns musicRecords
+        every { musicRepository.findById(musicIdToAdd) } returns Optional.of(musicToAdd)
+        every { musicRecordRepository.deleteAll(any<List<MusicRecord>>()) } just Runs
+        every { musicRecordRepository.saveAll(any<List<MusicRecord>>()) } returns emptyList()
 
-	@Test
-	void 음악_기록_하나를_캘린더_아이디로_조회한다() {
+        // when
+        musicRecordService.updateMusicRecords(calendarDateId, userId, newMusicIds)
 
-		// given
-		when(calendarDateRepository.findByIdOrThrow(calendarDateId)).thenReturn(calendarDate);
-		when(musicRecordRepository.findTopByCalendarDate(calendarDate)).thenReturn(
-			Optional.of(musicRecords.getFirst()));
-
-		// when
-		Optional<MusicRecord> result = musicRecordService.findOneByCalendarDateId(calendarDateId);
-
-		// then
-		assertThat(result).isPresent();
-		assertThat(result.get().getMusic().getId()).isEqualTo(musicRecords.getFirst().getMusic().getId());
-
-	}
-
-	@Test
-	void 음악_기록을_업데이트한다() {
-
-		// given
-		String commonMusicId = musicRecords.getFirst().getMusic().getId();
-		String musicIdToAdd = "3";
-		String musicNameToAdd = "Song 3";
-
-		when(calendarDateRepository.findWithOwnerByIdOrThrow(calendarDateId)).thenReturn(calendarDate);
-		when(userRepository.getById(userId)).thenReturn(user);
-		when(permissionService.checkPermission(any(User.class), any(User.class))).thenReturn(EDIT);
-		when(musicRecordRepository.findByCalendarDate(calendarDate)).thenReturn(musicRecords);
-		when(musicRepository.findByIdOrThrow(musicIdToAdd))
-			.thenReturn(Music.builder().id(musicIdToAdd).name(musicNameToAdd).build());
-
-		List<String> newMusicIds = List.of(commonMusicId, musicIdToAdd);
-
-		// when
-		musicRecordService.updateMusicRecords(calendarDateId, userId, newMusicIds);
-
-		// then
-		verify(musicRecordRepository).deleteAll(any());
-		verify(musicRecordRepository).saveAll(any());
-
-	}
-
+        // then
+        verify(exactly = 1) { musicRecordRepository.deleteAll(any<List<MusicRecord>>()) }
+        verify(exactly = 1) { musicRecordRepository.saveAll(any<List<MusicRecord>>()) }
+    }
 }
