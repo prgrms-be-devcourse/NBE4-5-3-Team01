7ad8de663d963a8ae62c3223d98f2534be4df2f0
shinwoos <kmnj2100@gmail.com>
2025-04-09 14:52:57 +0900

    feat. 린트수정

--- backend/src/test/java/com/team01/project/user/service/UserServiceTest.kt
+++ backend/src/test/java/com/team01/project/user/service/UserServiceTest.kt
@@ -15,7 +15,6 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.ArgumentMatchers.any
 import org.mockito.ArgumentMatchers.anyString
-import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.Mockito.`when`
 import org.mockito.junit.jupiter.MockitoExtension
