87504ab2a6700c03282ea2907fdfd4ac24eb7c2a
jisukimtech <jisukim.tech@gmail.com>
2025-04-09 20:13:48 +0900

    Refactor(PermissionServiceTest): 마이그레이션

--- backend/src/test/java/com/team01/project/permission/service/PermissionServiceTest.kt
+++ backend/src/test/java/com/team01/project/permission/service/PermissionServiceTest.kt
@@ -1,121 +1,89 @@
-package com.team01.project.permission.service;
+package com.team01.project.permission.service
 
-import static com.team01.project.domain.follow.entity.type.Status.*;
-import static com.team01.project.global.permission.CalendarPermission.*;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import com.team01.project.domain.follow.entity.type.Status
+import com.team01.project.domain.follow.repository.FollowRepository
+import com.team01.project.domain.user.entity.CalendarVisibility
+import com.team01.project.domain.user.entity.User
+import com.team01.project.global.permission.CalendarPermission
+import com.team01.project.global.permission.PermissionService
+import com.team01.project.util.TestDisplayNameGenerator
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.DisplayNameGeneration
+import org.junit.jupiter.api.Test
+import java.util.Optional
 
-import java.util.Optional;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import com.team01.project.domain.calendardate.repository.CalendarDateRepository;
-import com.team01.project.domain.follow.entity.type.Status;
-import com.team01.project.domain.follow.repository.FollowRepository;
-import com.team01.project.domain.user.entity.CalendarVisibility;
-import com.team01.project.domain.user.entity.User;
-import com.team01.project.global.permission.CalendarPermission;
-import com.team01.project.global.permission.PermissionService;
-
-@ExtendWith(MockitoExtension.class)
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@DisplayNameGeneration(TestDisplayNameGenerator::class)
 class PermissionServiceTest {
+    private val followRepository: FollowRepository = mockk()
+    private val permissionService = PermissionService(followRepository)
 
-	@Mock
-	private CalendarDateRepository calendarDateRepository;
+    private val calendarOwner = User(
+        id = "calendar owner",
+        email = "owner email",
+        name = "owner name",
+        calendarVisibility = CalendarVisibility.FOLLOWER_ONLY // 팔로워에게 공개
+    )
 
-	@Mock
-	private FollowRepository followRepository;
+    private val loggedInUser = User(
+        id = "logged in user",
+        email = "user email",
+        name = "user name"
+    )
 
-	@InjectMocks
-	private PermissionService permissionService;
+    @Test
+    fun `오너라서 캘린더를 수정할 권한이 있다`() {
+        // when
+        val calendarPermission = permissionService.checkPermission(calendarOwner, calendarOwner)
 
-	private User calendarOwner;
-	private User loggedInUser;
+        // then
+        assertEquals(CalendarPermission.EDIT, calendarPermission)
+    }
 
-	@BeforeEach
-	void 초기화() {
-		calendarOwner = User.builder()
-			.id("calendar owner")
-			.calendarVisibility(CalendarVisibility.FOLLOWER_ONLY) // 팔로워에게 공개
-			.build();
-		loggedInUser = User.builder()
-			.id("logged in user")
-			.build();
-	}
+    @Test
+    fun `오너가 아니라서 캘린더를 수정할 권한이 없다`() {
+        // given
+        every { followRepository.findStatusByToUserAndFromUser(calendarOwner, loggedInUser) } returns Optional.of(Status.ACCEPT)
 
-	@Test
-	void 오너라서_캘린더를_수정할_권한이_있다() {
+        // when
+        val calendarPermission = permissionService.checkPermission(calendarOwner, loggedInUser)
 
-		// when
-		CalendarPermission calendarPermission = permissionService.checkPermission(calendarOwner, calendarOwner);
+        // then
+        assertNotEquals(CalendarPermission.EDIT, calendarPermission)
+    }
 
-		// then
-		assertEquals(EDIT, calendarPermission);
+    @Test
+    fun `오너라서 캘린더를 조회할 권한이 있다`() {
+        // when
+        val calendarPermission = permissionService.checkPermission(calendarOwner, calendarOwner)
 
-	}
+        // then
+        assertNotEquals(CalendarPermission.NONE, calendarPermission)
+    }
 
-	@Test
-	void 오너가_아니라서_캘린더를_수정할_권한이_없다() {
+    @Test
+    fun `팔로워라서 팔로워 공개 캘린더를 조회할 권한이 있다`() {
+        // given
+        every { followRepository.findStatusByToUserAndFromUser(calendarOwner, loggedInUser) } returns Optional.of(Status.ACCEPT)
 
-		// given
-		when(followRepository.findStatusByToUserAndFromUser(any(User.class), any(User.class)))
-			.thenReturn(Optional.of(ACCEPT));
+        // when
+        val calendarPermission = permissionService.checkPermission(calendarOwner, loggedInUser)
 
-		// when
-		CalendarPermission calendarPermission = permissionService.checkPermission(calendarOwner, loggedInUser);
+        // then
+        assertEquals(CalendarPermission.VIEW, calendarPermission)
+    }
 
-		// then
-		assertNotEquals(EDIT, calendarPermission);
+    @Test
+    fun `캘린더를 조회할 권한이 없다`() {
+        // given
+        every { followRepository.findStatusByToUserAndFromUser(calendarOwner, loggedInUser) } returns Optional.of(Status.NONE)
 
-	}
+        // when
+        val calendarPermission = permissionService.checkPermission(calendarOwner, loggedInUser)
 
-	@Test
-	void 오너라서_캘린더를_조회할_권한이_있다() {
-
-		// when
-		CalendarPermission calendarPermission = permissionService.checkPermission(calendarOwner, calendarOwner);
-
-		// then
-		assertNotEquals(CalendarPermission.NONE, calendarPermission);
-
-	}
-
-	@Test
-	void 팔로워라서_팔로워_공개_캘린더를_조회할_권한이_있다() {
-
-		// given
-		when(followRepository.findStatusByToUserAndFromUser(any(User.class), any(User.class)))
-			.thenReturn(Optional.of(ACCEPT));
-
-		// when
-		CalendarPermission calendarPermission = permissionService.checkPermission(calendarOwner, loggedInUser);
-
-		// then
-		assertEquals(VIEW, calendarPermission);
-
-	}
-
-	@Test
-	void 캘린더를_조회할_권한이_없다() {
-
-		// given
-		when(followRepository.findStatusByToUserAndFromUser(any(User.class), any(User.class)))
-			.thenReturn(Optional.of(Status.NONE));
-
-		// when
-		CalendarPermission calendarPermission = permissionService.checkPermission(calendarOwner, loggedInUser);
-
-		// then
-		assertEquals(CalendarPermission.NONE, calendarPermission);
-
-	}
-
+        // then
+        assertEquals(CalendarPermission.NONE, calendarPermission)
+    }
 }
