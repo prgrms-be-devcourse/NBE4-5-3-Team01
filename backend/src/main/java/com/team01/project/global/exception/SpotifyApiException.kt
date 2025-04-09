5b7881407f3c9465aa20ad1660c0f219f7ca58f2
jxxngin <wwd0203@daum.net>
2025-04-09 10:29:12 +0900

    Refactor: SpotifyApiException 형식 변경

--- backend/src/main/java/com/team01/project/global/exception/SpotifyApiException.kt
+++ backend/src/main/java/com/team01/project/global/exception/SpotifyApiException.kt
@@ -1,6 +1,18 @@
 package com.team01.project.global.exception
 
-class SpotifyApiException : RuntimeException {
-    constructor(message: String) : super(message)
-    constructor(message: String, cause: Throwable) : super(message, cause)
+enum class SpotifyErrorCode(
+    val code: String,
+    val message: String
+) {
+    TRACK_NOT_FOUND("404-1", "트랙 정보를 찾을 수 없습니다."),
+    ARTIST_GENRE_NOT_FOUND("404-2", "아티스트 장르 정보를 찾을 수 없습니다."),
+    PLAYLIST_NOT_FOUND("404-3", "Playlist 정보를 찾을 수 없습니다."),
+    INVALID_REQUEST("400-1", "Spotify API 요청 오류 발생"),
+    SERVER_ERROR("500-1", "Spotify API 서버 오류 발생"),
+    UNKNOWN("500-2", "Spotify API 처리 중 알 수 없는 오류 발생")
 }
+
+class SpotifyApiException(
+    val errorCode: SpotifyErrorCode,
+    cause: Throwable? = null
+) : RuntimeException(errorCode.message, cause)
