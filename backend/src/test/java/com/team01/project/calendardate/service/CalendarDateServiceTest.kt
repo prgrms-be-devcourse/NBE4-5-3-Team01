14370297f2cee92157d08d9272365a6e3e18f556
jisukimtech <jisukim.tech@gmail.com>
2025-04-10 23:33:12 +0900

    Feat(CalendarDateServiceTest): 테스트 케이스 추가

--- backend/src/test/java/com/team01/project/calendardate/service/CalendarDateServiceTest.kt
+++ backend/src/test/java/com/team01/project/calendardate/service/CalendarDateServiceTest.kt
@@ -8,9 +8,12 @@ import com.team01.project.domain.calendardate.service.CalendarDateService
 import com.team01.project.domain.music.entity.Music
 import com.team01.project.domain.musicrecord.service.MusicRecordService
 import com.team01.project.domain.user.repository.UserRepository
+import com.team01.project.global.exception.CalendarDateAlreadyExistsException
+import com.team01.project.global.exception.PermissionDeniedException
 import com.team01.project.global.permission.CalendarPermission
 import com.team01.project.global.permission.PermissionService
 import com.team01.project.user.entity.UserFixture
+import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
@@ -42,21 +45,19 @@ class CalendarDateServiceTest: BehaviorSpec({
     val start = yearMonth.atDay(1)
     val end = yearMonth.atEndOfMonth()
 
-    val ownerId = "owner"
-    val owner = UserFixture.유저(ownerId)
-    val userId = "user"
-    val user = UserFixture.유저(userId)
+    val owner = UserFixture.유저("owner")
+    val user = UserFixture.유저("user")
 
     val calendarDate1Id = 1L
     val calendarDate1 = CalendarDateFixture.calendarDate(id = calendarDate1Id, user = owner, date = start)
-    val calendarDate2Id = 2L
-    val calendarDate2 = CalendarDateFixture.calendarDate(id = calendarDate2Id, user = owner, date = end)
+    val calendarDate2 = CalendarDateFixture.calendarDate(id = 2L, user = owner, date = end)
     val calendarDates = listOf(calendarDate1, calendarDate2)
 
+    // 공통 모킹
     beforeContainer {
         clearAllMocks()
-        every { userRepository.findById(ownerId) } returns Optional.of(owner)
-        every { userRepository.findById(userId) } returns Optional.of(user)
+        every { userRepository.findById(owner.id) } returns Optional.of(owner)
+        every { userRepository.findById(user.id) } returns Optional.of(user)
     }
 
     Given("유저가 캘린더 VIEW 권한을 가지는 경우") {
@@ -70,16 +71,16 @@ class CalendarDateServiceTest: BehaviorSpec({
             }
 
             val result by lazy {
-                calendarDateService.findAllByYearAndMonth(ownerId, userId, yearMonth)
+                calendarDateService.findAllByYearAndMonth(owner.id, user.id, yearMonth)
             }
 
             Then("해당 연도와 월의 모든 캘린더 데이터가 반환된다") {
                 result shouldHaveSize calendarDates.size
-                result.all { it.user.id == ownerId } shouldBe true
+                result.all { it.user.id == owner.id } shouldBe true
             }
         }
 
-        When("캘린더 아이디로 캘린더를 조회하면") {
+        And("캘린더 아이디에 해당하는 캘린더가 존재하는 상태에서") {
             val musics = emptyList<Music>()
             val response = CalendarDateFetchResponse.of(calendarDate1, musics, CalendarPermission.VIEW)
 
@@ -88,33 +89,84 @@ class CalendarDateServiceTest: BehaviorSpec({
                 every { musicRecordService.findMusicsByCalendarDateId(calendarDate1Id) } returns musics
             }
 
-            val result by lazy {
-                calendarDateService.findCalendarDateWithMusics(calendarDate1Id, userId)
+            When("캘린더 아이디로 캘린더를 조회하면") {
+                val result by lazy {
+                    calendarDateService.findCalendarDateWithMusics(calendarDate1Id, user.id)
+                }
+
+                Then("해당 아이디의 캘린더 데이터가 반환된다") {
+                    result shouldBe response
+                }
+            }
+        }
+    }
+
+    Given("유저가 캘린더 VIEW 권한을 가지지 않은 경우") {
+        beforeTest {
+            every { permissionService.checkPermission(eq(owner), eq(user)) } returns CalendarPermission.NONE
+        }
+
+        When("연도와 월로 먼슬리 캘린더를 조회하면") {
+            Then("PermissionDeniedException이 발생한다") {
+                shouldThrow<PermissionDeniedException> {
+                    calendarDateService.findAllByYearAndMonth(owner.id, user.id, yearMonth)
+                }
+            }
+        }
+
+        When("캘린더 아이디로 캘린더를 조회하면") {
+            beforeTest {
+                every { calendarDateRepository.findWithOwnerById(calendarDate1Id) } returns Optional.of(calendarDate1)
             }
 
-            Then("해당 아이디의 캘린더 데이터가 반환된다") {
-                result shouldBe response
+            Then("PermissionDeniedException이 발생한다") {
+                shouldThrow<PermissionDeniedException> {
+                    calendarDateService.findCalendarDateWithMusics(calendarDate1Id, user.id)
+                }
             }
         }
     }
 
     Given("유저가 캘린더 EDIT 권한을 가지는 경우") {
+        val newMemo = "new memo"
+
         beforeTest {
             every { permissionService.checkPermission(eq(owner), eq(user)) } returns CalendarPermission.EDIT
+        }
+
+        And("캘린더 아이디에 해당하는 캘린더가 존재하는 상태에서") {
+            beforeTest {
+                every { calendarDateRepository.findWithOwnerById(calendarDate1Id) } returns Optional.of(calendarDate1)
+            }
+
+            When("캘린더 메모를 수정하면") {
+                val result by lazy {
+                    calendarDateService.updateMemo(calendarDate1Id, user.id, newMemo)
+                }
+
+                Then("메모 내용이 변경된다") {
+                    result
+                    calendarDate1.memo shouldBe newMemo
+                }
+
+            }
+        }
+    }
+
+    Given("유저가 캘린더 EDIT 권한을 가지지 않은 경우") {
+        val newMemo = "new memo"
+
+        beforeTest {
             every { calendarDateRepository.findWithOwnerById(calendarDate1Id) } returns Optional.of(calendarDate1)
+            every { permissionService.checkPermission(eq(owner), eq(user)) } returns CalendarPermission.VIEW
         }
 
         When("캘린더 메모를 수정하면") {
-            val newMemo = "new memo"
-            val result by lazy {
-                calendarDateService.updateMemo(calendarDate1Id, userId, newMemo)
+            Then("PermissionDeniedException이 발생한다") {
+                shouldThrow<PermissionDeniedException> {
+                    calendarDateService.updateMemo(calendarDate1Id, user.id, newMemo)
+                }
             }
-
-            Then("메모 내용이 변경된다") {
-                result
-                calendarDate1.memo shouldBe newMemo
-            }
-
         }
     }
 
@@ -128,15 +180,54 @@ class CalendarDateServiceTest: BehaviorSpec({
         }
 
         When("유저 아이디로 캘린더를 생성하면") {
-            val result by lazy {
-                calendarDateService.create(userId, date, memo)
-            }
+            val result = calendarDateService.create(user.id, date, memo)
 
             Then("생성 후 저장된 캘린더 데이터가 반환된다") {
                 result shouldNotBe null
-                result.user.id shouldBe userId
+                result.user.id shouldBe user.id
                 result.date shouldBe date
                 result.memo shouldBe memo
+            }
+        }
+    }
+
+    Given("캘린더가 이미 존재하는 날짜에") {
+        val date = LocalDate.now()
+        val memo = "memo"
+
+        beforeTest {
+            every { calendarDateRepository.existsByUserAndDate(user, date) } returns true
+        }
+
+        When("유저 아이디로 캘린더를 생성하면") {
+            Then("CalendarDateAlreadyExistsException이 발생한다") {
+                shouldThrow<CalendarDateAlreadyExistsException> {
+                    calendarDateService.create(user.id, date, memo)
+                }
+            }
+        }
+    }
+
+    Given("캘린더가 존재하지 않는 경우") {
+        beforeTest {
+            every { calendarDateRepository.findWithOwnerById(calendarDate1Id) } returns Optional.empty()
+        }
+
+        When("캘린더 아이디로 캘린더를 조회하면") {
+            Then("IllegalArgumentException이 발생한다") {
+                shouldThrow<IllegalArgumentException> {
+                    calendarDateService.findCalendarDateWithMusics(calendarDate1Id, user.id)
+                }
+            }
+        }
+
+        When("캘린더 메모를 수정하면") {
+            val newMemo = "new memo"
+
+            Then("IllegalArgumentException이 발생한다") {
+                shouldThrow<IllegalArgumentException> {
+                    calendarDateService.updateMemo(calendarDate1Id, user.id, newMemo)
+                }
             }
         }
     }
