98e444fc405f4ea71df8583213cb01da13b28434
sameom1048 <samvision99@naver.com>
2025-04-10 14:04:50 +0900

    :recycle: Refactor: klint refactoring

--- backend/src/main/java/com/team01/project/domain/user/service/UserEmailService.kt
+++ backend/src/main/java/com/team01/project/domain/user/service/UserEmailService.kt
@@ -5,7 +5,6 @@ import jakarta.mail.internet.MimeMessage
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.mail.MailException
 import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.mail.javamail.MimeMessageHelper
 import org.springframework.stereotype.Service
 
 @Service
