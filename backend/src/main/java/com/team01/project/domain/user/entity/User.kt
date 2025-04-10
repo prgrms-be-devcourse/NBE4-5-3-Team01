713e0cc1ff6006d34944c8726945615a8d59b257
shinwoos <kmnj2100@gmail.com>
2025-04-10 19:21:13 +0900

    feat. 유저 엔티티, dto 수정

--- backend/src/main/java/com/team01/project/domain/user/entity/User.kt
+++ backend/src/main/java/com/team01/project/domain/user/entity/User.kt
@@ -16,6 +16,7 @@ import lombok.AllArgsConstructor
 import lombok.Builder
 import lombok.Getter
 import lombok.NoArgsConstructor
+import org.springframework.data.jpa.domain.AbstractPersistable_.id
 import java.time.LocalDate
 import java.time.LocalDateTime
 
@@ -53,6 +54,8 @@ open class User(
 
     open var field: String? = null,
 
+    open var loginPlatform: String? = null,
+
     @Column(name = "calendar_visibility", nullable = false)
     @Enumerated(EnumType.STRING)
     open var calendarVisibility: CalendarVisibility = CalendarVisibility.PUBLIC,
@@ -65,6 +68,7 @@ open class User(
 
     @OneToOne(mappedBy = "user", cascade = [CascadeType.ALL], orphanRemoval = true)
     open var membership: Membership? = null
+
 ) {
 
     fun updateCalendarVisibility(newCalendarVisibility: CalendarVisibility) {
