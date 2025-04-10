925552d150222b87e470f13d0b664d67fdec90da
jisukimtech <jisukim.tech@gmail.com>
2025-04-10 21:10:49 +0900

    Refactor(SimpleUserResponse): Kotlin 마이그레이션

--- backend/src/main/java/com/team01/project/domain/user/dto/SimpleUserResponse.kt
+++ backend/src/main/java/com/team01/project/domain/user/dto/SimpleUserResponse.kt
@@ -1,23 +1,27 @@
-package com.team01.project.domain.user.dto;
+package com.team01.project.domain.user.dto
 
-import com.team01.project.domain.user.entity.CalendarVisibility;
-import com.team01.project.domain.user.entity.User;
+import com.team01.project.domain.user.entity.CalendarVisibility
+import com.team01.project.domain.user.entity.User
 
-public record SimpleUserResponse(
-	String id,
-	String name,
-	String profileImg,
-	String originalName,
-	CalendarVisibility calendarVisibility
+/**
+ * 유저 데이터 응답 DTO
+ */
+data class SimpleUserResponse(
+    val id: String,
+    val name: String,
+    val profileImg: String?,
+    val originalName: String?,
+    val calendarVisibility: CalendarVisibility
 ) {
-
-	public static SimpleUserResponse from(User user) {
-		return new SimpleUserResponse(
-			user.getId(),
-			user.getName(),
-			user.getImage(),
-			user.getOriginalName(),
-			user.getCalendarVisibility()
-		);
-	}
+    companion object {
+        fun from(user: User): SimpleUserResponse {
+            return SimpleUserResponse(
+                user.id,
+                user.name,
+                user.image,
+                user.originalName,
+                user.calendarVisibility
+            )
+        }
+    }
 }
