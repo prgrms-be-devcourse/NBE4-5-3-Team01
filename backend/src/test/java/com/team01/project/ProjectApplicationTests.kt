af6568dfe026670245b5fedef9108cf969710392
sameom1048 <samvision99@naver.com>
2025-04-10 21:08:56 +0900

    :recycle: Refactor: ProjectApplicationTests kt migration

--- backend/src/test/java/com/team01/project/ProjectApplicationTests.kt
+++ backend/src/test/java/com/team01/project/ProjectApplicationTests.kt
@@ -1,13 +1,12 @@
-package com.team01.project;
+package com.team01.project
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
