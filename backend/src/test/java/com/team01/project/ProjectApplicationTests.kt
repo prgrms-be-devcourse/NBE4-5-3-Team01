6086a18393ac099346c273eca8f62057f614e8a1
jxxngin <wwd0203@daum.net>
2025-04-10 20:56:20 +0900

    Refactor: ProjectApplicationTests Migration

--- backend/src/test/java/com/team01/project/ProjectApplicationTests.kt
+++ backend/src/test/java/com/team01/project/ProjectApplicationTests.kt
@@ -1,13 +1,13 @@
-package com.team01.project;
+package com.team01.project
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class ProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+        // Spring Boot context가 정상적으로 로드되는지만 확인하는 테스트입니다.
+    }
 }
