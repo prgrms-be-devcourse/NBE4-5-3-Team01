076d964f18e89a883894472f22dbcb550c849e81
jisukimtech <jisukim.tech@gmail.com>
2025-04-10 11:05:46 +0900

    Refactor(UserFixture): Kotlin 마이그레이션

--- backend/src/test/java/com/team01/project/user/entity/UserFixture.kt
+++ backend/src/test/java/com/team01/project/user/entity/UserFixture.kt
@@ -1,14 +1,13 @@
-package com.team01.project.user.entity;
+package com.team01.project.user.entity
 
-import com.team01.project.domain.user.entity.User;
+import com.team01.project.domain.user.entity.User
 
-public class UserFixture {
+object UserFixture {
+    fun 유저(id: String): User {
+        return User(id = id, name = "이름", email = "test@gamil.com")
+    }
 
-	public static User 유저(String id) {
-		return User.builder().id(id).name("이름").email("test@gmail.com").build();
-	}
-
-	public static User 유저_이메일(String id, String email) {
-		return User.builder().id(id).name("이름").email(email).build();
-	}
+    fun 유저_이메일(id: String, email: String): User {
+        return User(id = id, name = "이름", email = email)
+    }
 }
